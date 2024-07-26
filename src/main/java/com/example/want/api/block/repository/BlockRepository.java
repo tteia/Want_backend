873b1ddce20bb293064b6b75638e0b1fa02c0906
jsangmin99 <jsangmin99@gmail.com>
2024-07-26 12:38:24 +0900

    블록 리스트 조회 수정, 활성화 되어있는지 아닌지, 좋아요 순으로 정렬, 시작 시간 순으로 정렬

--- src/main/java/com/example/want/api/block/repository/BlockRepository.java
+++ src/main/java/com/example/want/api/block/repository/BlockRepository.java
@@ -1,14 +1,18 @@
 package com.example.want.api.block.repository;
-
 import com.example.want.api.block.domain.Block;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 @Repository
 public interface BlockRepository extends JpaRepository<Block, Long> {
 
-    Page<Block> findAllByIsActivated(String y, Pageable pageable);
+    Page<Block> findAllByIsActivatedOrderByStartTimeAsc(@Param("isActivated") String Y, Pageable pageable);
+    Page<Block> findAllByIsActivatedOrderByHeartCountDesc (@Param("isActivated") String N, Pageable pageable);
+
 }
