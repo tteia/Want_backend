79de3de7c04e88493208592bcf8067201b07c098
vydndrl <vydndrl1234@naver.com>
2024-07-28 11:44:56 +0900

    초대 기능

---
+++ src/main/java/com/example/want/api/project/dto/InvitationDto.java
@@ -0,0 +1,12 @@
+package com.example.want.api.project.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class InviteUserDto {
+    private String email;
+}
