71bef8f609f6e321d3d004d06bf5b6f9c7871590
vydndrl <vydndrl1234@naver.com>
2024-07-28 11:47:08 +0900

    초대 기능 누락 부분 추가

--- src/main/java/com/example/want/api/member/repository/MemberRepository.java
+++ src/main/java/com/example/want/api/member/repository/MemberRepository.java
@@ -10,4 +10,6 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
 
     boolean existsByEmail(String email);
+
+    Member findMemberByEmail(String email);
 }
--- src/main/java/com/example/want/api/project/dto/InvitationDto.java
+++ src/main/java/com/example/want/api/project/dto/InvitationDto.java
@@ -7,6 +7,6 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class InviteUserDto {
+public class InvitationDto {
     private String email;
 }
