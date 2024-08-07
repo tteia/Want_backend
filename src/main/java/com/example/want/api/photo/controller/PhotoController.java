cafc235dab977e24d7aeefd73c01d6f8c9606b8d
keemzleun <92363679+keemzleun@users.noreply.github.com>
2024-08-07 19:26:02 +0900

    jeonsangmin 님이 변경한 내용

--- src/main/java/com/example/want/api/photo/controller/PhotoController.java
+++ src/main/java/com/example/want/api/photo/controller/PhotoController.java
@@ -1,25 +1,19 @@
 package com.example.want.api.photo.controller;
 
-import com.example.want.api.photo.dto.CreatePhotoRqDto;
 
-import com.example.want.api.photo.domain.Photo;
+import com.example.want.api.photo.dto.PhotoListRsDto;
 import com.example.want.api.photo.service.PhotoService;
 import com.example.want.common.CommonResDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -33,19 +27,21 @@ public class PhotoController {
     public ResponseEntity<Object> photoUpload(@RequestPart Long blockId, @RequestPart(value = "files", required = false) List<MultipartFile> files){
         // 입력된 파일의 개수가 10개 이하인지 판별
         if (files.size() > 10) {
-            response.put("message", "There are too many files to upload.");
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+            return new ResponseEntity<>(new CommonResDto(HttpStatus.BAD_REQUEST, "There are too many files to upload", null), HttpStatus.BAD_REQUEST);
         }
         try {
             List<PhotoListRsDto.PhotoInfoDto> infoDtos = new ArrayList<>();
             for (MultipartFile file : files){
-                 photoService.uploadFile(file);
+                PhotoListRsDto.PhotoInfoDto photoInfoDto = photoService.uploadFile(blockId, file);  // 이미지 파일 id, url
+                infoDtos.add(photoInfoDto);
             }
-            response.put("message", "Successfully uploaded " + files.size() + " files.");
-            return ResponseEntity.ok(response);
+            PhotoListRsDto photoListRsDto = PhotoListRsDto.builder()
+                    .blockId(blockId)
+                    .photoList(infoDtos)
+                    .build();
+            return new ResponseEntity<>(new CommonResDto(HttpStatus.OK, "Successfully uploaded " + files.size() + " files.", photoListRsDto), HttpStatus.OK);
         } catch (IOException e) {
-            response.put("message", "Failed to upload files.");
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+            return new ResponseEntity<>(new CommonResDto(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to upload files.", null), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
