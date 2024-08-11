f252097ece22920c6ea68f8f27c7b52996cd9ccf
keemzleun <92363679+keemzleun@users.noreply.github.com>
2024-08-11 14:11:22 +0900

    memberService 수정

--- src/main/java/com/example/want/api/member/service/MemberService.java
+++ src/main/java/com/example/want/api/member/service/MemberService.java
@@ -69,7 +69,7 @@ public class MemberService {
             throw new IllegalArgumentException("Uncorrected invitation code");
         }
 
-        if (dto.getAction().equals("Y")) {
+        if (dto.getAction().equals("accept")) {
             if (projectMember.getInvitationAccepted().equals("Y")) {
                 throw new IllegalArgumentException("Invitation already accepted");
             }
