7b652d7dff4dcf02fbd5a298d43ccbae575768d6
jeonsangmin <jsangmin99@gmail.com>
2024-08-15 17:46:15 +0900

    댓글서비스에 UserInfo 추가 및 각종 검증 추가

--- src/main/java/com/example/want/api/block_comment/service/CmtService.java
+++ src/main/java/com/example/want/api/block_comment/service/CmtService.java
@@ -9,6 +9,9 @@ import com.example.want.api.block_comment.domain.Cmt;
 import com.example.want.api.block_comment.repository.CmtRepository;
 import com.example.want.api.member.domain.Member;
 import com.example.want.api.member.repository.MemberRepository;
+import com.example.want.api.project.repository.ProjectRepository;
+import com.example.want.api.projectMember.Repository.ProjectMemberRepository;
+import com.example.want.api.projectMember.domain.ProjectMember;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -24,17 +27,22 @@ public class CmtService {
     private final CmtRepository cmtRepository;
     private final MemberRepository memberRepository;
     private final BlockRepository blockRepository;
+    private final ProjectMemberRepository projectMemberRepository;
+    private final ProjectRepository projectRepository;
 
     // Create
     @Transactional
     public Cmt create(CreateCmtRqDto dto, String email){
-        Member member = memberRepository.findByEmail(email).orElseThrow(() -> new EntityNotFoundException("member is not found"));
-        Block block = blockRepository.findById(dto.getBlockId()).orElseThrow(()->new EntityNotFoundException("block is not found"));
+        Member member = findMemberByEmail(email);
+        Block block = findBlockById(dto.getBlockId());
         return cmtRepository.save( dto.toEntity(member, block));
     }
 
     // Read
-    public Page<CmtRsDto> cmtList(Pageable pageable, Long blockId){
+    public Page<CmtRsDto> cmtList(Pageable pageable, Long blockId, String email){
+        Member member = findMemberByEmail(email);
+        Block block = findBlockById(blockId);
+        ProjectMember projectMember = checkProjectMember(block, member);
         Page<Cmt> cmts = cmtRepository.findByBlockIdAndIsDeleted(pageable, blockId, "N");
         Page<CmtRsDto> dtos = cmts.map(a -> a.listFromEntity());
         return dtos;
@@ -42,17 +50,34 @@ public class CmtService {
 
     // Update
     @Transactional
-    public void update(CmtUpdateRqDto dto){
+    public void update(CmtUpdateRqDto dto, String email){
+        Member member = findMemberByEmail(email);
         Cmt cmt = cmtRepository.findById(dto.getCommentId()).orElseThrow(()->new EntityNotFoundException("존재하지 않는 댓글 아이디입니다."));
-        cmt.updateCmt(dto);
+        Block block = cmt.getBlock();
+        ProjectMember projectMember = checkProjectMember(block, member);
+        cmt.updateCmt(dto.getContents());
     }
 
     // Delete
     @Transactional
-    public void delete(Long id){
-        // isDeleted = 1로 변경
-        Cmt cmt = cmtRepository.findById(id).orElseThrow(()->new EntityNotFoundException("존재하지 않는 댓글 아이디입니다."));
+    public void delete(Long commentId, String email){
+        Member member = findMemberByEmail(email);
+        Cmt cmt = cmtRepository.findById(commentId).orElseThrow(()->new EntityNotFoundException("존재하지 않는 댓글 아이디입니다."));
+        Block block = cmt.getBlock();
+        ProjectMember projectMember = checkProjectMember(block, member);
+        // 댓글 삭제
         cmt.deleteCmt("Y");
+    }
+
+    public ProjectMember checkProjectMember(Block block, Member member){
+        return projectMemberRepository.findByProjectAndMember(block.getProject(), member).orElseThrow(()->new EntityNotFoundException("project member is not found"));
+    }
+
+    public Member findMemberByEmail(String email){
+        return memberRepository.findByEmail(email).orElseThrow(()->new EntityNotFoundException("member is not found"));
+    }
+    public Block findBlockById(Long blockId){
+        return blockRepository.findById(blockId).orElseThrow(()->new EntityNotFoundException("block is not found"));
     }
 
 }
